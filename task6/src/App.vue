94fecfd070fabcc5f34ad3174c53f935b6108c43
Leonardo Oliveros <leonardoliveros.lo@gmail.com>
2021-09-18 06:44:35 -0500

    Add task 6

---
+++ task6/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <active-user
+            :username="username"
+            :age="age"
+        ></active-user>
+        <user-data
+            @save="saveData"
+        ></user-data>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            age: 0
+        }
+    },
+    methods: {
+        saveData(username, age) {
+            this.username = username
+            this.age = age
+        }
+    }
+}
+</script>
---
+++ task6/src/components/UserData.vue
@@ -0,0 +1,32 @@
+<template>
+    <form @submit.prevent="saveData">
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username" v-model="enteredUsername" />
+        </div>
+        <div>
+            <label for="age">Age:</label>
+            <input type="number" name="age" id="age" v-model="enteredAge" />
+        </div>
+        <button>Submit</button>
+    </form>
+</template>
+
+<script>
+export default {
+    events: ['save'],
+    data() {
+        return {
+            enteredUsername: '',
+            enteredAge: 0
+        };
+    },
+    methods: {
+        saveData() {
+            if (this.enteredUsername != '' && this.enteredAge > 0) {
+                this.$emit('save', this.enteredUsername, this.enteredAge);
+            }
+        }
+    }
+}
+</script>
